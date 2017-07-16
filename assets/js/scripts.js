a0589b171dab28ff8034058fb10491d0ca1fd7de
ArialBlack <andreyturik@gmail.com>
2017-07-17 00:17:44 +0300

    many fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,6 @@
 (function(window, document, $) {
   'use strict';
 
-
-
-  
-
   $(window).load(function() {
     // Toggle menu
     $.app.menu.toggle();
@@ -14,9 +10,4 @@
     },100);
   });
 
-
-
 })(window, document, jQuery);
-
-//vertical-layout vertical-menu 2-columns fixed-navbar collapsed  menu-collapsed pace-done
-//vertical-layout vertical-menu 2-columns fixed-navbar collapsed  menu-collapsed pace-done
